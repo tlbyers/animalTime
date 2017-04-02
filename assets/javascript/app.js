1e1b3475e425f9359af08155b887d97fba73f991
Terri Byers <Terri Byers>
2017-04-02 12:34:08 -0400

    submit7

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -57,7 +57,7 @@
       var animal = $(this).attr("data-name");
 
       // Constructing a queryURL using the animal name
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       // Performing an AJAX request with the queryURL
